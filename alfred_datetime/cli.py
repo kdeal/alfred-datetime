c211f1aa1e0ca5dc0ecec085e98b87077c5e0973
Kyle Deal <kdeal@kyledeal.com>
2017-09-10 14:59:47 -0700

    Handle human time strings and break parsing out more
    Strings are:
    - now
    - today
    - tomorrow
    - yesterday
    - days of the week

--- alfred_datetime/cli.py
+++ alfred_datetime/cli.py
@@ -1,22 +1,55 @@
 import json
 import sys
+from collections import OrderedDict
 from traceback import print_exc
 
 import pendulum
 from pendulum.parsing.exceptions import ParserError
 
 ALFRED_TIME_FORMATS = (
+    'to_datetime_string',
     'isoformat',
     'to_date_string',
     'to_time_string',
     'to_formatted_date_string',
-    'to_datetime_string',
-    '%A %-d%t of %B %Y %I:%M:%S %p',
+    'to_rfc2822_string',
 )
+# Use OrderedDict so there a priority to partial matches
+TIME_STRINGS = OrderedDict({
+    'now': pendulum.now,
+    'today': pendulum.today,
+    'tomorrow': pendulum.tomorrow,
+    'yesterday': pendulum.yesterday,
+    'monday': lambda: day_of_week(pendulum.MONDAY),
+    'tuesday': lambda: day_of_week(pendulum.TUESDAY),
+    'wednesday': lambda: day_of_week(pendulum.WEDNESDAY),
+    'thursday': lambda: day_of_week(pendulum.THURSDAY),
+    'friday': lambda: day_of_week(pendulum.FRIDAY),
+    'saturday': lambda: day_of_week(pendulum.SATURDAY),
+    'sunday': lambda: day_of_week(pendulum.SUNDAY),
+})
+
+
+def day_of_week(day_of_week):
+    today = pendulum.today()
+    if today.day_of_week == day_of_week:
+        return today
+    return today.next(day_of_week)
+
+
+def parse_time(query):
+    query = ' '.join(query)
+    for time_string, time in TIME_STRINGS.items():
+        if time_string.startswith(query.lower()):
+            return time()
+    try:
+        return pendulum.from_timestamp(int(query))
+    except ValueError:
+        return pendulum.parse(query)
 
 
 def apply_formats(time, formats=ALFRED_TIME_FORMATS):
-    formats = {}
+    formats = OrderedDict()
     for time_format in ALFRED_TIME_FORMATS:
         func = getattr(time, time_format, None)
         if func:
@@ -48,10 +81,11 @@ def alfredify_error(error):
 
 def generate_items(query):
     try:
-        time = pendulum.parse(' '.join(query))
+        time = parse_time(query)
     except ParserError as error:
         print_exc()
         return alfredify_error(error)
+
     formatted = apply_formats(time)
     return alfredify_times(formatted)
 
